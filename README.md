a432b1410e77c2314077a5660bbd75b594cc2765
Dakota <dakotawkeast@gmail.com>
2024-08-13 19:03:28 -0400

    add readme, refactored some code, added additonal choices

---
+++ README.md
@@ -0,0 +1,17 @@
+# My Awesome Project
+A Pick-Your-Own Adventure game based in the terminal where you try and survive Shia LeBeouf! This was heavily inspired by the song by Rob Cantor called 'Shia LeBeouf' (https://www.youtube.com/watch?v=o0u4M6vppCI)
+
+
+## How It's Made:
+
+**Tech used:** Python
+
+Here's where you can go to town on how you actually built this thing. Write as much as you can here, it's totally fine if it's not too much just make sure you write *something*. If you don't have too much experience on your resume working on the front end that's totally fine. This is where you can really show off your passion and make up for that ten fold.
+
+## Lessons Learned:
+
+I learned a lot about how to interact with the terminal and make a story seem better formatted, especially when it came to showing the options the reader could take.
+
+## Examples:
+Take a look at these couple examples that I have in my own portfolio:
+
--- main.py
+++ main.py
@@ -29,9 +29,14 @@ class Killer:
 # initiate class. able to affect Shia LeBeouf's health this way'
 killer = Killer()
 
+# function to display choices
+def choices(choices):
+    for num, option in choices.items():
+        print("{} : {}".format(num, option))
+
 
 clear()
-answer = input(f'Ready you ready to try and survive the mighty {killer.name}?! y/n ')
+answer = input(f'Ready you ready to try and survive the mighty {killer.name}?! (y/n) ')
 
 if answer != 'y':
     exit()
@@ -56,15 +61,14 @@ choice_one = {
     'd': "stand really still and hope he doesn't see you"
 }
 
-for num, option in choice_one.items():
-    print("{} : {}".format(num, option))
+choices(choice_one)
 
 print('----------------------------------------------------------------')
 
 answer_one = None
 
 while answer_one not in choice_one.keys():
-    answer_one = input("What do you do? ")
+    answer_one = input("What do you do?: ")
 
 clear()
 print("You decided to {}".format(choice_one[answer_one]))
@@ -87,3 +91,62 @@ elif answer_one == 'd':
 print("You break into a sprint and search for your car but you're all turned around.")
 print("He's almost upon you now and you see his face is covered in blood. My God, there's blood everywhere!")
 print("You give up looking for your car and just focus on outrunning Hollywood superstar {}, who just brandished a knife behind you.".format(killer.name))
+print("As you're running for your life from {}, you start wondering why he's doing out here in the woods, why he seems to be killing for fun, and why there seems to be human flesh stuck between his teeth. Was {} an actual cannibal?".format(killer.name, killer.name))
+
+sleep(15)
+clear()
+
+print("After running for what seems like forever, you stop when you notice that he's no longer behind you. It's dark but you seem to have lost him.")
+print("But you're hopelessly lost yourself. Afraid that he may be nearby still, you silently creep through the underbrush.")
+print("After a few minutes of walking, you spot a small cottage with a light on inside.")
+print("You heave a sigh of relief and start to hope that this nightmare will soon be over. You start to walk towards the cottage.")
+
+sleep(10)
+clear()
+
+print("Suddenly, you feel a sharp pain in your right leg! You look down and notice that you have your foot stuck in a bear trap!")
+
+choice_two = {
+    'a': 'scream as loudly as possible',
+    'b': 'call out for help',
+    'c': 'gnaw off your leg',
+    'd': 'try to remove the bear trap'
+}
+
+count = 0
+
+choices(choice_two)
+
+print('----------------------------------------------------------------')
+
+answer_two = None
+
+while answer_two not in choice_two.keys():
+    answer_two = input("What do you do?: ")
+
+# What happens if user inputs 'n' before the count reaches 3? TODO: fix bug where if user hits 'n' and then answers question about what choice they want to try next, it doesn't display anything.
+if answer_two == 'd':
+    ans = 'y'
+    while ans == 'y' and count < 3:
+        clear()
+        ans = input("It doesn't work. Do you want to try again? (y/n): ")
+        count += 1
+        if ans == 'n':
+            choices(choice_two)
+            answer_two = None
+
+    if ans == 'y' and count == 3:
+        print("You took too long and bled out!")
+        print("Game over!")
+        exit()
+    while answer_two not in choice_two.keys():
+        answer_two = input("What do you do?: ")
+elif answer_two == 'b':
+    print("{} comes skipping merrily out of the cottage and comes straight for you. You're not sure what's scarier: death or having your last sight be of a grown man drenched in blood skipping happily through a field of floweres.".format(killer.name))
+    print("Game over!")
+    exit()
+elif answer_two == 'a':
+    print("As you curse every God you can think of and the person that planted this trap, you fail to notice {} come running out of the cottage. You may be dead but screaming was actually kind of a nice stress reliever.".format(killer.name))
+    print("Game over!")
+    exit()
+
